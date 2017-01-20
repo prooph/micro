d42ebb5162a7c7f9a078b73028b36ab9cf74579b
prolic <saschaprolic@googlemail.com>
2017-01-20 21:38:40 +0800

    add AmqpPublisher

---
+++ src/AmqpPublisher.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the prooph/micro.
+ * (c) 2017-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2017-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\Micro\AmqpPublisher;
+
+use Prooph\Common\Messaging\Message;
+use Prooph\Common\Messaging\MessageConverter;
+use Prooph\Common\Messaging\MessageDataAssertion;
+
+const buildPublisher = 'Prooph\Micro\AmqpProducer\buildPublisher';
+
+function buildPublisher (\AMQPChannel $channel, MessageConverter $messageConverter, string $exchangeName): callable
+{
+    if (! $channel->isConnected()) {
+        throw new \RuntimeException('Provided AMQP channel is not connected');
+    }
+
+    $exchange = new \AMQPExchange($channel);
+    $exchange->setName($exchangeName);
+
+    return function (Message $message) use ($messageConverter, $exchange) {
+        $messageData = $this->messageConverter->convertToArray($message);
+        MessageDataAssertion::assert($messageData);
+        $messageData['created_at'] = $message->createdAt()->format('Y-m-d\TH:i:s.u');
+
+        $exchange->publish($messageData, $message->messageName(), \AMQP_NOPARAM, [
+            'timestamp' => $message->createdAt()->getTimestamp(),
+            'type' => $message->messageType(),
+        ]);
+    };
+}
