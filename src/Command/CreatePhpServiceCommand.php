de8bc475a9f73c6d5541fedc07378d369e0aec57
Bradley Weston <brad@legalweb.org.uk>
2017-01-24 19:22:45 +0000

    Different syntax to remove GOTO and also made sure a release always happens after a lock.

--- src/Command/CreatePhpServiceCommand.php
+++ src/Command/CreatePhpServiceCommand.php
@@ -50,78 +50,48 @@ class CreatePhpServiceCommand extends AbstractCommand
             return 1;
         }
 
-        if (! file_exists($this->getRootDir() . '/docker-compose.yml')) {
-            $io->warning('docker-compose.yml does not exist. Run ./bin/micro micro:setup. Aborted.');
+        try {
+            if (! file_exists($this->getRootDir() . '/docker-compose.yml')) {
+                $io->warning('docker-compose.yml does not exist. Run ./bin/micro micro:setup. Aborted.');
 
-            $this->release();
+                $this->release();
 
-            return 1;
-        }
-
-        $currentConfig = Yaml::parse(file_get_contents($this->getRootDir() . '/docker-compose.yml'));
-
-        $question = new Question('Name of the service: ');
-        $question->setValidator(function ($answer) {
-            if (! is_string($answer) || ! preg_match('/^[a-z-0-9]+$/', $answer)) {
-                throw new \RuntimeException(
-                    'Invalid service name'
-                );
+                return 1;
             }
 
-            return $answer;
-        });
-        $question->setMaxAttempts(2);
+            $currentConfig = Yaml::parse(file_get_contents($this->getRootDir() . '/docker-compose.yml'));
 
-        $serviceName = $io->askQuestion($question);
+            $question = new Question('Name of the service: ');
+            $question->setValidator(function ($answer) {
+                if (! is_string($answer) || ! preg_match('/^[a-z-0-9]+$/', $answer)) {
+                    throw new \RuntimeException(
+                        'Invalid service name'
+                    );
+                }
 
-        $question = new ChoiceQuestion('Which PHP image to use?', [
-            'prooph/php:7.1-cli',
-            'prooph/php:7.1-fpm',
-            'prooph/php:7.0-cli',
-            'prooph/php:7.0-fpm',
-            'prooph/php:5.6-cli',
-            'prooph/php:5.6-fpm',
-        ]);
-        $question->setMaxAttempts(2);
+                return $answer;
+            });
+            $question->setMaxAttempts(2);
 
-        $image = $io->askQuestion($question);
+            $serviceName = $io->askQuestion($question);
 
-        $question = new Question('Give the directory in which to mount the service (defaults to: "/var/www")', '/var/www');
-        $question->setValidator(function ($answer) {
-            if (! is_string($answer) || strlen($answer) === 0) {
-                throw new \RuntimeException(
-                    'Invalid service name'
-                );
-            }
+            $question = new ChoiceQuestion('Which PHP image to use?', [
+                'prooph/php:7.1-cli',
+                'prooph/php:7.1-fpm',
+                'prooph/php:7.0-cli',
+                'prooph/php:7.0-fpm',
+                'prooph/php:5.6-cli',
+                'prooph/php:5.6-fpm',
+            ]);
+            $question->setMaxAttempts(2);
 
-            return $answer;
-        });
-        $question->setMaxAttempts(2);
+            $image = $io->askQuestion($question);
 
-        $volume = $io->askQuestion($question);
-
-        $services = array_merge(['[NONE]'], array_keys($currentConfig['services']));
-        $question = new ChoiceQuestion('On which services is the new service dependend? (comma separated)', $services);
-        $question->setMultiselect(true);
-
-        $dependsOn = $io->askQuestion($question);
-
-        if ($dependsOn === ['[NONE]']) {
-            $dependsOn = [];
-        }
-
-        $useNginxConfig = false;
-
-        if ('-fpm' === substr($image, -4, 4)) {
-            $useNginxConfig = true;
-
-            $io->section('Nginx configuration');
-            upstream:
-            $question = new Question('Add upstream (f.e. "php-user-GET"): ');
+            $question = new Question('Give the directory in which to mount the service (defaults to: "/var/www")', '/var/www');
             $question->setValidator(function ($answer) {
                 if (! is_string($answer) || strlen($answer) === 0) {
                     throw new \RuntimeException(
-                        'Invalid upstream'
+                        'Invalid service name'
                     );
                 }
 
@@ -129,155 +99,181 @@ class CreatePhpServiceCommand extends AbstractCommand
             });
             $question->setMaxAttempts(2);
 
-            $upStreams[] = $io->askQuestion($question);
+            $volume = $io->askQuestion($question);
 
-            if ($io->confirm('Add more upstreams?', false)) {
-                goto upstream;
+            $services = array_merge(['[NONE]'], array_keys($currentConfig['services']));
+            $question = new ChoiceQuestion('On which services is the new service dependend? (comma separated)', $services);
+            $question->setMultiselect(true);
+
+            $dependsOn = $io->askQuestion($question);
+
+            if ($dependsOn === ['[NONE]']) {
+                $dependsOn = [];
             }
 
-            nginxlocation:
-            $question = new Question('Add location (f.e. "= /api/v1/user-$request_method"): ');
-            $question->setValidator(function ($answer) {
-                if (! is_string($answer) || 0 === strlen($answer)) {
-                    throw new \RuntimeException(
-                        'Invalid location'
-                    );
+            $useNginxConfig = false;
+
+            if ('-fpm' === substr($image, -4, 4)) {
+                $useNginxConfig = true;
+
+                $io->section('Nginx configuration');
+                do {
+                    $question = new Question('Add upstream (f.e. "php-user-GET"): ');
+                    $question->setValidator(function ($answer) {
+                        if (! is_string($answer) || strlen($answer) === 0) {
+                            throw new \RuntimeException(
+                                'Invalid upstream'
+                            );
+                        }
+
+                        return $answer;
+                    });
+                    $question->setMaxAttempts(2);
+
+                    $upStreams[] = $io->askQuestion($question);
+                } while ($io->confirm('Add more upstreams?', false));
+
+                do {
+                    $question = new Question('Add location (f.e. "= /api/v1/user-$request_method"): ');
+                    $question->setValidator(function ($answer) {
+                        if (! is_string($answer) || 0 === strlen($answer)) {
+                            throw new \RuntimeException(
+                                'Invalid location'
+                            );
+                        }
+
+                        return $answer;
+                    });
+                    $question->setMaxAttempts(2);
+
+                    $locations[] = $io->askQuestion($question);
+
+                    $question = new ChoiceQuestion('Add fastcgi_pass: ', $upStreams);
+
+                    $fastcgiPasses[] = $io->askQuestion($question);
+
+                    $question = new Question('Add fastcgi_index (defaults to "index.php"): ', 'index.php');
+                    $fastcgiIndexes[] = $io->askQuestion($question);
+
+                    $question = new Question('Add fastcgi_param SCRIPT_FILENAME (defaults to "\'/var/www/public/index.php\': "): ', '\'/var/www/public/index.php\'');
+                    $scriptFileNames[] = $io->askQuestion($question);
+
+                    $question = new Question('Add fastcgi_param PATH_INFO (defaults to "\'/\'"): ', '\'/\'');
+                    $pathInfos[] = $io->askQuestion($question);
+
+                    $question = new Question('Add fastcgi_param SCRIPT_NAME (defaults to "\'/index.php\'"): ', '\'/index.php\'');
+                    $scriptNames[] = $io->askQuestion($question);
+                } while ($io->confirm('Do you want to add another nginx location?', false));
+            } else {
+                $question = new Question('Enter the start command (f.e. "php run.php"): ', '');
+                $question->setValidator(function ($answer) {
+                    if (! is_string($answer) || strlen($answer) === 0) {
+                        throw new \RuntimeException(
+                            'Invalid command'
+                        );
+                    }
+
+                    return $answer;
+                });
+                $question->setMaxAttempts(2);
+
+                $command = $io->askQuestion($question);
+
+                $restart = $io->confirm('Always restart container if it exists?', false);
+            }
+
+            $dependsOnString = '';
+            foreach ($it = new \CachingIterator(new \ArrayIterator($dependsOn), \CachingIterator::FULL_CACHE) as $dependend) {
+                $dependsOnString .= "  - $dependend";
+                if ($it->hasNext()) {
+                    $dependsOnString .= "\n";
+                }
+            }
+
+            if ($useNginxConfig) {
+                $gatewayFile = $this->getRootDir() . '/gateway/www.conf';
+
+                $parser = new Parser();
+
+                $nginxConfig = $parser->parseFile($gatewayFile);
+
+                $builder = new Builder();
+                $builder->append($nginxConfig);
+
+                $servers = $nginxConfig->search(function (Node $node) {
+                    return $node instanceof Server;
+                });
+
+                $count = count($servers);
+
+                if ($count === 0) {
+                    throw new \RuntimeException('No server section found in gateway config.');
                 }
 
-                return $answer;
-            });
-            $question->setMaxAttempts(2);
-
-            $locations[] = $io->askQuestion($question);
-
-            $question = new ChoiceQuestion('Add fastcgi_pass: ', $upStreams);
-
-            $fastcgiPasses[] = $io->askQuestion($question);
-
-            $question = new Question('Add fastcgi_index (defaults to "index.php"): ', 'index.php');
-            $fastcgiIndexes[] = $io->askQuestion($question);
-
-            $question = new Question('Add fastcgi_param SCRIPT_FILENAME (defaults to "\'/var/www/public/index.php\': "): ', '\'/var/www/public/index.php\'');
-            $scriptFileNames[] = $io->askQuestion($question);
-
-            $question = new Question('Add fastcgi_param PATH_INFO (defaults to "\'/\'"): ', '\'/\'');
-            $pathInfos[] = $io->askQuestion($question);
-
-            $question = new Question('Add fastcgi_param SCRIPT_NAME (defaults to "\'/index.php\'"): ', '\'/index.php\'');
-            $scriptNames[] = $io->askQuestion($question);
-
-            if ($io->confirm('Do you want to add another nginx location?', false)) {
-                goto nginxlocation;
-            }
-        } else {
-            $question = new Question('Enter the start command (f.e. "php run.php"): ', '');
-            $question->setValidator(function ($answer) {
-                if (! is_string($answer) || strlen($answer) === 0) {
-                    throw new \RuntimeException(
-                        'Invalid command'
-                    );
+                if ($count > 1) {
+                    throw new \RuntimeException('More than one server section found in gateway config.');
                 }
 
-                return $answer;
-            });
-            $question->setMaxAttempts(2);
+                $server = $servers->getIterator()->current();
 
-            $command = $io->askQuestion($question);
+                /* @var Server $server */
+                foreach ($upStreams as $upStream) {
+                    $builder->append(new Upstream(new Param($upStream), [
+                        new Directive('server', [new Param("$serviceName:9000")]),
+                    ]));
+                }
 
-            $restart = $io->confirm('Always restart container if it exists?', false);
-        }
+                while ($location = array_shift($locations)) {
+                    $fastcgiPass = array_shift($fastcgiPasses);
+                    $fastcgiIndex = array_shift($fastcgiIndexes);
+                    $scriptFileName = array_shift($scriptFileNames);
+                    $pathInfo = array_shift($pathInfos);
+                    $scriptName = array_shift($scriptNames);
 
-        $dependsOnString = '';
-        foreach ($it = new \CachingIterator(new \ArrayIterator($dependsOn), \CachingIterator::FULL_CACHE) as $dependend) {
-            $dependsOnString .= "  - $dependend";
-            if ($it->hasNext()) {
-                $dependsOnString .= "\n";
-            }
-        }
+                    $server->append(new Location(new Param($location), null, [
+                        new Directive('fastcgi_split_path_info', [new Param('^(.+\.php)(/.+)$')]),
+                        new Directive('fastcgi_pass', [new Param($fastcgiPass)]),
+                        new Directive('fastcgi_index', [new Param($fastcgiIndex)]),
+                        new Directive('include', [new Param('fastcgi_params')]),
+                        new Directive('fastcgi_param', [new Param('SCRIPT_FILENAME ' . $scriptFileName)]),
+                        new Directive('fastcgi_param', [new Param('PATH_INFO ' . $pathInfo)]),
+                        new Directive('fastcgi_param', [new Param('SCRIPT_NAME ' . $scriptName)]),
+                    ]));
+                }
 
-        if ($useNginxConfig) {
-            $gatewayFile = $this->getRootDir() . '/gateway/www.conf';
-
-            $parser = new Parser();
-
-            $nginxConfig = $parser->parseFile($gatewayFile);
-
-            $builder = new Builder();
-            $builder->append($nginxConfig);
-
-            $servers = $nginxConfig->search(function (Node $node) {
-                return $node instanceof Server;
-            });
-
-            $count = count($servers);
-
-            if ($count === 0) {
-                throw new \RuntimeException('No server section found in gateway config.');
+                $builder->dumpFile($gatewayFile);
             }
 
-            if ($count > 1) {
-                throw new \RuntimeException('More than one server section found in gateway config.');
-            }
-
-            $server = $servers->getIterator()->current();
-
-            /* @var Server $server */
-            foreach ($upStreams as $upStream) {
-                $builder->append(new Upstream(new Param($upStream), [
-                    new Directive('server', [new Param("$serviceName:9000")]),
-                ]));
-            }
-
-            while ($location = array_shift($locations)) {
-                $fastcgiPass = array_shift($fastcgiPasses);
-                $fastcgiIndex = array_shift($fastcgiIndexes);
-                $scriptFileName = array_shift($scriptFileNames);
-                $pathInfo = array_shift($pathInfos);
-                $scriptName = array_shift($scriptNames);
-
-                $server->append(new Location(new Param($location), null, [
-                    new Directive('fastcgi_split_path_info', [new Param('^(.+\.php)(/.+)$')]),
-                    new Directive('fastcgi_pass', [new Param($fastcgiPass)]),
-                    new Directive('fastcgi_index', [new Param($fastcgiIndex)]),
-                    new Directive('include', [new Param('fastcgi_params')]),
-                    new Directive('fastcgi_param', [new Param('SCRIPT_FILENAME ' . $scriptFileName)]),
-                    new Directive('fastcgi_param', [new Param('PATH_INFO ' . $pathInfo)]),
-                    new Directive('fastcgi_param', [new Param('SCRIPT_NAME ' . $scriptName)]),
-                ]));
-            }
-
-            $builder->dumpFile($gatewayFile);
-        }
-
-        $config = [
-            'services' => [
-                $serviceName => [
-                    'image' => $image,
-                    'volumes' => [
-                        "./service/$serviceName:$volume",
+            $config = [
+                'services' => [
+                    $serviceName => [
+                        'image' => $image,
+                        'volumes' => [
+                            "./service/$serviceName:$volume",
+                        ],
                     ],
                 ],
-            ],
-        ];
+            ];
 
-        if (! empty($dependsOn)) {
-            $config['services'][$serviceName]['depends_on'] = $dependsOn;
+            if (! empty($dependsOn)) {
+                $config['services'][$serviceName]['depends_on'] = $dependsOn;
+            }
+
+            if (isset($restart)) {
+                $config['services'][$serviceName]['restart'] = 'always';
+            }
+
+            if (isset($command)) {
+                $config['services'][$serviceName]['command'] = $command;
+            }
+
+            $this->updateConfig($serviceName, $config);
+
+            $io->success('Successfully updated microservice settings');
+
+            return 0;
+        } finally {
+            $this->release();
         }
-
-        if (isset($restart)) {
-            $config['services'][$serviceName]['restart'] = 'always';
-        }
-
-        if (isset($command)) {
-            $config['services'][$serviceName]['command'] = $command;
-        }
-
-        $this->updateConfig($serviceName, $config);
-
-        $io->success('Successfully updated microservice settings');
-
-        $this->release();
-
-        return 0;
     }
 }
